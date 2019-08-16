e6c42412f6149701387083a956b7d8f69c1f54f8
Fang <thesunskys@gmail.com>
2019-08-16 12:26:14 +0700

    edit model table product productop

---
+++ src/controller/PreorderController.js
@@ -0,0 +1,43 @@
+const con = require('../config/config')
+
+const Preorder = {
+    async getProduct (req, res) {
+        const selectAll = 'SELECT proid,proname FROM product'
+        let resp = []
+        try {
+            const value = await con.pool.query(selectAll)
+            for ( let i = 0; i < (value.rows).length; i++) {
+                let obj = {
+                    order : i+1,
+                    productid : value.rows[i].proid,
+                    productname : value.rows[i].proname
+                }
+                resp.push(obj)
+            }
+            return res.status(200).send(resp)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    async getProductDetail (req, res) {
+        const key = req.params.id
+        const selectOne =   `SELECT proopid,sku,price,optionvalue from productoption
+                            WHERE proid = $1`
+        try { 
+            const result = await con.pool.query(selectOne,[key])
+            return res.status(200).send(result.rows)
+        } catch (error) {   
+            console.log(error)
+        }
+    },
+    async insertPreorder (req, res) {
+        const value = req.body
+        try {
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+module.exports = {Preorder}
--- src/controller/ProductController.js
+++ src/controller/ProductController.js
@@ -2,17 +2,62 @@ const con = require('../config/config')
 
 const Product = {
     //GET POPUP
-async getPopup(req,res){
-    
-    const getProduct = ''; //picture|productname|price
-    const getOptionvalue = ''; //optionname|optionvalue
-    const getProductOption = '';//sku|price
-
-    try{
+    async getPopup(req,res){
         
-    }catch(error){
-        return res.status(400).send({'message':'error'});
-    }
+        const getProduct = ''; //picture|productname|price
+        const getOptionvalue = ''; //optionname|optionvalue
+        const getProductOption = '';//sku|price
+
+        try{
+            
+        }catch(error){
+            return res.status(400).send({'message':'error'});
+        }
+    },
+    async getMaxMin(req, res) {
+        try {
+            const selectMin =   `SELECT pro.proid,pro.proname,proop.price 
+                                    FROM product pro 
+                                    FULL JOIN productoption proop 
+                                    ON pro.proid = proop.proid 
+                                WHERE price = (
+                                    SELECT DISTINCT 
+                                    MIN (price) 
+                                    FROM productoption 
+                                    WHERE proid  = $1)`
+             const selectMax =  `SELECT pro.proid,pro.proname,proop.price 
+                                    FROM product pro 
+                                    FULL JOIN productoption proop 
+                                    ON pro.proid = proop.proid 
+                                WHERE price = (
+                                    SELECT DISTINCT 
+                                    MAX (price) 
+                                    FROM productoption 
+                                    WHERE proid  = $1)`
+            const selectProduct = 'select proid,proname from product' 
+            const result = await con.pool.query(selectProduct) 
+            let sumValue = []
+            let price,allValue
+            for (let i = 0; i < (result.rows).length; i++) {
+                const id = result.rows[i].proid
+                const queryMax = await con.pool.query(selectMax,[id])
+                const queryMin = await con.pool.query(selectMin,[id])
+                const max = queryMax.rows[i].price
+                const min = queryMin.rows[i].price
+                const proName = result.rows[i].proname
+                price = min + ' - ' + max
+                allValue = {
+                    order : i+1,
+                    proid : id,
+                    proname : proName,
+                    price : price
+                }
+                sumValue.push(allValue)
+            }
+            return res.status(200).send(sumValue)
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 
