199376e8cd2e7d4e98ac8f13383b5b59a7b21d86
apisitprompha <apisitprompha@gmail.com>
2019-10-10 16:24:52 +0700

    Dashboard

--- src/services/Dashboard.js
+++ src/services/Dashboard.js
@@ -6,18 +6,6 @@ const Response = require('../lib/Reposnce');
 const helper = require('../lib/Helper');
 const productoptions = require('./options'); 
 
-// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ SELLER $$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
-async function topproduct (req, res, next) {
-    const sql = ``
-    const value = []
-    try {
-        return Response.resSuccess(res, successMessage);
-    } catch (error) {
-        return Response.resError(res, errorMessage.saveError);
-    }
-}
-
 // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ ADMIN $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 async function topseller (req, res, next) {
