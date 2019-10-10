e7ee2dc7a5e31d0f4a63e80f2abb789a86c69cb8
apisitprompha <apisitprompha@gmail.com>
2019-10-10 16:23:54 +0700

    shipping/edit

--- src/routes/ShippingRoute.js
+++ src/routes/ShippingRoute.js
@@ -6,6 +6,6 @@ router.get('/recieve', shipping.recieve); // ADMIN
 
 router.post('/recieve', shipping.customerreceive); // CUSTOMER
 router.post('/seller/shipping', shipping.sellershipping); // SELLER
-router.post('/edit', shipping.edit);
+router.post('/edit', shipping.edit); // EDIT SHIPPING TRACK
 
 module.exports = router;
