9762a6961cbba150b36b5edc4caa6a4f33422b49
Fang <thesunskys@gmail.com>
2019-10-10 17:20:12 +0700

    add api admin

--- src/routes/AdminRoute.js
+++ src/routes/AdminRoute.js
@@ -1,25 +1,22 @@
 const router = require('express').Router();
 const admin = require('../services/Admin');
 
-<<<<<<< HEAD
 const { 
     updatePaymentStatus, 
     updateSellerStatus, 
-    login, 
-    getUserData, 
+    login,
     createAdmin, 
-    getUserOneData, 
     sellers,
     orders
  }= require('../services/AdminService')
-=======
-router.get('/',(req,res)=>{res.json('ADMIN ROUTE')});
->>>>>>> master
+ 
+router.get('/', (req, res) => {
+    res.json({ info: `welcome to admin` })
+});
 
 router.post('/add',admin.add);
 router.post('/login', admin.login);
 
-<<<<<<< HEAD
 router.get('/orders', orders)
 router.get('/orders/:id', orders)
 router.get('/sellers', sellers)
@@ -32,6 +29,3 @@ router.post('/login', login)
 router.post('/register', createAdmin)
 
 module.exports = router
-=======
-module.exports = router;
->>>>>>> master
