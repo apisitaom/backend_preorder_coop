d87da6e554ea8711072f7f7b2b031ea6e6719b43
apisitprompha <apisitprompha@gmail.com>
2019-10-11 14:56:50 +0700

    เปลี่ยนชื่อ function service/member

--- src/routes/MemberRoute.js
+++ src/routes/MemberRoute.js
@@ -4,10 +4,10 @@ const img = require('../lib/ImageUpload');
 const auth = require('../lib/Auth');
 
 router.get('/', (req, res, next)=>{res.json('MEMBER ROUTE')});
-router.get('/lists',auth.userVerifyToken ,member.getProfileMember);
+router.get('/lists',auth.userVerifyToken ,member.list);
 
-router.post('/add',img.upload, member.registerMember);
-router.post('/edit',auth.userVerifyToken,img.upload,member.updateMember);
-router.post('/login',member.logInMember);
+router.post('/add',img.upload, member.add);
+router.post('/edit',auth.userVerifyToken,img.upload,member.edit);
+router.post('/login',member.login);
 
 module.exports = router;
